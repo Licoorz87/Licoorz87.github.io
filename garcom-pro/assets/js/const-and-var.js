8c2445bb8ce5c77abeaaefffd1731aee5a7ef2b5
Licoorz87 <82511992+Licoorz87@users.noreply.github.com>
2024-10-31 13:40:00 -0300

    Add files via upload
    Beta 1.0.0 (Beta version, refactoring required)

---
+++ garcom-pro/assets/js/const-and-var.js
@@ -0,0 +1,23 @@
+// Const & Var
+const DICT = "0123456789abcdefghijklmnopqsrtuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var newOrder;
+var toggleNewOrderButton;
+var newOrderShowBool;
+var trash;
+
+var typePayment = 0;
+var toggleNewOrderTypePayment;
+var newOrderTypePaymentShowBool;
+var newOrderTypePaymentBg;
+var newOrderTypePaymentItem;
+var newOrderTypePaymentSelectSpan;
+
+var commissionCheck;
+
+var newOrderKeyboardKey;
+
+var newOrderVisorValue;
+var value = 0;
+
+var submitButton;
