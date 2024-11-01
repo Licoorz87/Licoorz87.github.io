f597fc63b32ef432999292b4409bbeed424697eb
Licoorz87 <82511992+Licoorz87@users.noreply.github.com>
2024-11-01 00:48:43 -0300

    Update download.js

--- garcom-pro/assets/js/download.js
+++ garcom-pro/assets/js/download.js
@@ -28,7 +28,7 @@ function download() {
 
     const a = document.createElement('a');
     a.href = url;
-    a.download = `${filename}.gpro`;
+    a.download = `${filename}.gpro.txt`;
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
