15708e3391e33668e0990e1ead54d48fab07d210
Licoorz87 <82511992+Licoorz87@users.noreply.github.com>
2024-11-01 00:39:17 -0300

    Add files via upload

---
+++ garcom-pro/assets/js/const-and-var.js
@@ -0,0 +1,21 @@
+// Const & Var
+var newOrder;
+var toggleNewOrderButton;
+var newOrderShowBool;
+var trash;
+
+var typePayment = 0;
+var toggleNewOrderTypePayment;
+var newOrderTypePaymentShowBool;
+var newOrderTypePaymentBg;
+var newOrderTypePaymentItem;
+var newOrderTypePaymentSelectSpan;
+
+var commissionCheck;
+
+var newOrderKeyboardKey;
+
+var newOrderVisorValue;
+var value = 0;
+
+var submitButton;
